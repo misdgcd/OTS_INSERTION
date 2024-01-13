0f83d55a88b4505cd1f4fdab3236d8619bfa1222
misdgcd <mis@dgcd.com.ph>
2024-01-13 13:26:19 +0800

    january 13 2024

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SoHeaderModule } from './so-header/so-header.module';
+import { SoDetailsModule } from './so-details/so-details.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -21,7 +22,7 @@ import { SoHeaderModule } from './so-header/so-header.module';
       trustServerCertificate: true
     }
   
-  }), SoHeaderModule]
+  }), SoHeaderModule, SoDetailsModule]
   ,
   controllers: [AppController],
   providers: [AppService],
